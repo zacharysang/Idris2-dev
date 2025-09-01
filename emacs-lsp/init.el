03c6d200ab8635044ab35efddba67fe47efea73c
Zachary Sang <zacharysang@gmail.com>
2025-08-31 21:58:51 -0700

    Keybindings

--- emacs-lsp/init.el
+++ emacs-lsp/init.el
@@ -5,6 +5,9 @@
       (require 'lsp-mode)
       (message "lsp-mode loaded successfully")
       
+      ;; Set LSP keymap prefix
+      (setq lsp-keymap-prefix "M-l")
+       
       ;; Define minimal idris2 major mode
       (define-derived-mode idris2-mode prog-mode "Idris2"
         "Major mode for Idris2 files.")
@@ -25,5 +28,15 @@
       (add-to-list 'auto-mode-alist '("\\.idr\\'" . idris2-mode))
       
       ;; Auto-activation
-      (add-hook 'idris2-mode-hook #'lsp))
+      (add-hook 'idris2-mode-hook #'lsp)
+      
+      ;; Keybindings for idris2-lsp capabilities (browser-safe)
+      (define-key idris2-mode-map (kbd "M-g d") 'lsp-find-definition)      ; Go to definition
+      (define-key idris2-mode-map (kbd "M-g r") 'lsp-find-references)      ; Find references  
+      (define-key idris2-mode-map (kbd "M-g i") 'lsp-find-implementation)  ; Go to implementation
+      (define-key idris2-mode-map (kbd "M-h") 'lsp-describe-thing-at-point) ; Hover info
+      (define-key idris2-mode-map (kbd "M-r") 'lsp-rename)                 ; Rename symbol
+      (define-key idris2-mode-map (kbd "M-a") 'lsp-execute-code-action)    ; Code actions (case split, expr search, etc.)
+      (define-key idris2-mode-map (kbd "M-f") 'lsp-format-buffer)          ; Format buffer
+      (define-key idris2-mode-map (kbd "M-e") 'lsp-ui-flycheck-list))       ; Show errors
   (error (message "lsp-mode failed to load: %s" (error-message-string err))))
